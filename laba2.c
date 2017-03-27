ebd826c37a8db9f46f509d8d3e75d87bfd66422b
Lera-admin <lerasimonova54@mail.ru>
2017-03-27 18:13:47 +0700

    Data input

--- laba2.c
+++ laba2.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(){
     int sum,srok,itog,month; 
+    printf("%s","Vvedite summy vklada\n");
+    scanf("%d",&sum);
+    printf("%s","Vvedite srok vklada\n");
+    scanf("%d",&srok);
 return 0;
 }
