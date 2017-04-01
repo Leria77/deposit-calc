9c61bc0c749febdef9d77119d3d3ac2f0e5e56a1
Симонова Валерия Владимировна <mm51s15@awp12.csc.local>
2017-04-01 13:10:34 +0700

    Fix code 2

--- laba2.c
+++ laba2.c
@@ -14,22 +14,22 @@ int main()
         if(sum <= 100000) {
          
            if (srok <= 30)
-            itog = sum * month / 100 * 90;
+               itog = sum * month / 100 * 90;
            if (srok <= 120 && srok >= 31)
-            itog = sum * month / 100 * 102;
+               itog = sum * month / 100 * 102;
            if (srok <= 240 && srok >= 121)
-            itog = sum * month / 100 * 106;
+               itog = sum * month / 100 * 106;
            if (srok <= 365 && srok >= 241)
-            itog = sum * month / 100 * 112;
+               itog = sum * month / 100 * 112;
         } else {	
            if (srok <= 30)
-            itog = sum * month / 100 * 90;
+               itog = sum * month / 100 * 90;
            if (srok <= 120 && srok >= 31)
-            itog = sum * month / 100 * 103;
+               itog = sum * month / 100 * 103;
            if (srok <= 240 && srok >= 121)
-            itog = sum * month / 100 * 108;
+               itog = sum * month / 100 * 108;
            if (srok <= 365 && srok >= 241)
-            itog = sum * month / 100 * 115;
+               itog = sum * month / 100 * 115;
         }
         printf("%s", "Summa vklada=");
         printf("%d", itog);
